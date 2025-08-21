48821188c6236a560023e0c3331de3740e1315de
Anton <aipazukhin@yandex.ru>
2025-08-21 22:22:56 +0300

    test234234

--- _10_functions/lesson.py
+++ _10_functions/lesson.py
@@ -19,4 +19,4 @@ print(glas('aeiouyAEIOUY'))
 
 def format_date (*, day: int, month: str)->str:
     return f'The date is {day} of {month}'
-print(format_date(day=1, month='may1'))
+print(format_date(day=1, month='may1232tyuty'))
